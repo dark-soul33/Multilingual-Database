80271b29fe1295eb6701e2094ff461a8876296c8
hemadeepika421 <cse220001047@iiti.ac.in>
2024-07-15 13:08:16 +0530

    files

--- multilingual/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
-]
